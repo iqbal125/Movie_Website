56993cd41fdb9d70f6a68795133a0f9a76eff06e
iqbal125 <iqbal125@yahoo.com>
2017-01-27 13:35:59 -0800

    Add files via upload

---
+++ Movie Website/media.py
@@ -0,0 +1,16 @@
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """Creates movies objects with title, storyline, image, and trailer"""
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+
+
+#Toy Story 2
+#Zootopia
+#Monsters Inc
+#Fight Club
+#The Dark Knight
+#The Mask
