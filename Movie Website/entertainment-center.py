ecb231eb2aeb1ad22f1d9195c75412900d7f195f
iqbal125 <iqbal125@yahoo.com>
2017-02-25 19:56:10 -0800

    Add files via upload

--- Movie Website/entertainment-center.py
+++ Movie Website/entertainment-center.py
@@ -1,22 +1,27 @@
 import media
 import fresh_tomatoes
 
-
+#Stores the Movie Class instance of Toy Story 2
 Toy_Story_2 = media.Movie("Toy Story 2", "THe toys go on an adventure to find Woody", "https://upload.wikimedia.org/wikipedia/en/c/c0/Toy_Story_2.jpg",
                                                 "https://www.youtube.com/watch?v=Lu0sotERXhI")
+#Stores the Movie Class instance of Zootopia
 Zootopia = media.Movie("Zootopia", "A bunny becomes a police officer and teams up with a fox", "https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
                                     "https://www.youtube.com/watch?v=jWM0ct-OLsM")
+#Stores the Movie Class instance of Monsters Inc.
 Monsters_inc = media.Movie("Monsters Inc.", "Monsters use childrens screams for power generation", "https://upload.wikimedia.org/wikipedia/en/6/63/Monsters_Inc.JPG",
                                         "https://www.youtube.com/watch?v=cvOQeozL4S0")
+#Stores the Movie Class instance of Fight Club
 Fight_Club = media.Movie("Fight Club", "An insomniac starts hallucinating and creates a cult", "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
                                         "https://www.youtube.com/watch?v=SUXWAEX2jlg")
+#Stores the Movie Class instance of The Dark Knight
 The_DarK_Knight = media.Movie("The Dark Knight", "Batman vs The Joker", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
                                             "https://www.youtube.com/watch?v=EXeTwQWrcwY")
+#Stores the Movie Class instance of The Mask
 The_Mask = media.Movie("The Mask", "A anvergae guy discovers a magical mask", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM3NDI5OTA5Nl5BMl5BanBnXkFtZTgwNzE4ODYxMTE@._V1_UY1200_CR86,0,630,1200_AL_.jpg",
                                     "https://www.youtube.com/watch?v=hOqVRwGVUkA")
 
 #Array of Movies
 movies = [Toy_Story_2, Zootopia, Monsters_inc, Fight_Club, The_DarK_Knight, The_Mask]
 
-#Creates the Webpage with the movies array 
+#Creates the Webpage with the movies array
 fresh_tomatoes.open_movies_page(movies)
--- Movie Website/media.py
+++ Movie Website/media.py
@@ -1,13 +1,12 @@
+#Creates movies objects with title, storyline, image, and trailer
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        """Creates movies objects with title, storyline, image, and trailer"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-
-
+""" List of Movies """
 #Toy Story 2
 #Zootopia
 #Monsters Inc
