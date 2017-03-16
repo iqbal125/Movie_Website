1ee207e0d0d3932d1bf50236043280b772717ce5
iqbal125 <iqbal125@yahoo.com>
2017-03-16 16:20:41 -0700

    Add files via upload

--- Movie Website/media.py
+++ Movie Website/media.py
@@ -1,6 +1,7 @@
 #Creates movies objects with title, storyline, image, and trailer
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """ Create a Movie class instance """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
